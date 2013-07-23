74a7bb63afd91d923f9597e9675c968801efb24a
Jonathan Marmor <jmarmor@gmail.com>
2013-07-22 22:59:46 -0400

    Score, metadata, parts, instruments.

--- dedalus.py
+++ dedalus.py
@@ -1,53 +1,49 @@
+"""A piece of music for Dedalus ensemble.
+
+http://www.youtube.com/watch?v=_syDjTX90zA&feature=share&list=PL477C973BF13F8BBE
+
+"""
+
+import datetime
+
 import music21
 
-INSTRUMENTS = [
-    {
-        'name': 'Flute',
-        'score_order': 1,
-        'short_name': 'fl',
-        'musician_name': ''
-    },
-    {
-        'name': 'Soprano Saxophone',
-        'score_order': 2,
-        'short_name': 'ss',
-        'musician_name': ''
-    },
-    {
-        'name': 'Viola',
-        'score_order': 3,
-        'short_name': 'vla',
-        'musician_name': ''
-    },
-    {
-        'name': 'Cello',
-        'score_order': 4,
-        'short_name': 'vc',
-        'musician_name': ''
-    },
-    {
-        'name': 'Trombone',
-        'score_order': 5,
-        'short_name': 'tbn',
-        'musician_name': ''
-    },
-    {
-        'name': 'Guitar',
-        'score_order': 6,
-        'short_name': 'gtr',
-        'musician_name': 'Didier Aschour'
-    }
-]
 
-
-def main():
+def get_score():
     score = music21.stream.Score()
-    for instrument in INSTRUMENTS:
+
+    md = music21.metadata.Metadata()
+    md.title = 'Music for Dedalus Ensemble'
+    md.composer = 'Jonathan Marmor'
+    md.date = datetime.datetime.utcnow().strftime('%Y/%m/%d')
+    md.groupTitle = 'Dedalus'
+    md.dedication = 'Didier Aschour'
+    score.insert(0, md)
+
+    fl = music21.instrument.Flute()
+    sax = music21.instrument.SopranoSaxophone()
+    vla = music21.instrument.Viola()
+    vc = music21.instrument.Violoncello()
+    tbn = music21.instrument.Trombone()
+    gtr = music21.instrument.ElectricGuitar()
+
+    instruments = [fl, sax, vla, vc, tbn, gtr]
+    parts = []
+    for i in instruments:
         part = music21.stream.Part()
-        part.id = instrument['name']
+        part.insert(0, i)
+        part.id = i.instrumentAbbreviation
         score.insert(0, part)
-    score.show('text')
+        parts.append(part)
+
+    score.insert(0, music21.layout.StaffGroup(parts))
+
+    return score
+
+
+# def add_phrase(score):
 
 
 if __name__ == '__main__':
-    main()
+    score = get_score()
+    score.show()
